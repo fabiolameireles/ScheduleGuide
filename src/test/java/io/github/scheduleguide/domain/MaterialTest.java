a6697ac8ce0189a8a9447942a516913a4801324d
fabiolameireles <fabiola.vilaca@edu.ufes.br>
2026-07-13 20:32:30 -0300

    fix: ajustando testes de acordo com novo construtor da classe Material

--- src/test/java/io/github/scheduleguide/domain/MaterialTest.java
+++ src/test/java/io/github/scheduleguide/domain/MaterialTest.java
@@ -1,6 +1,10 @@
 package io.github.scheduleguide.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import java.util.Optional;
 
 
@@ -8,12 +12,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MaterialTest{
 
+    @Mock
+    private Conteudo mockConteudo;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     // Testando o Optional para link existente
     @Test
     public void testGetLinkQuandoLinkEstaPresente(){
         String linkEsperado = "https://link.com/teste";
     
-        Material material = new Material("Material", linkEsperado);
+        Material material = new Material("Material", mockConteudo, linkEsperado);
         
         Optional<String> linkRetornado = material.getLink();
         
@@ -25,10 +37,22 @@ public class MaterialTest{
     // Testando Optional para link que NÃO existe (null)
     @Test
     public void testGetLinkQuandoLinkEhNulo(){
-        Material material = new Material("Material");
+        Material material = new Material("Material", mockConteudo);
         
         Optional<String> linkRetornado = material.getLink();
         
         assertTrue(linkRetornado.isEmpty());
+    }
+
+    // Testando Construtor (valores padrão, com @JsonCreator)
+
+    @Test
+    public void testConstrutorPadrao() {
+        Material material = new Material();
+        
+        assertEquals("", material.getNome(), "O nome padrão deve ser uma string vazia");
+        assertNull(material.getConteudo(), "O conteúdo padrão deve ser nulo");
+        
+        assertTrue(material.getLink().isEmpty());
     }
 }

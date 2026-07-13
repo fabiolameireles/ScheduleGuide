443966818dd385221191b8174be2764bd5158f0a
IWBittencourt <igor.bittencourt@edu.ufes.br>
2026-07-13 10:58:37 -0300

    feat: ajusta código preexistente a inclui deleção de Categoria e Topico

--- src/main/resources/static/topicos.js
+++ src/main/resources/static/topicos.js
@@ -2,35 +2,37 @@ import "./axios.min.js"
 
 const novaCatModal = document.getElementById('novaCat');
 const editarCatModal = document.getElementById('editarCat');
+const deletarCatModal = document.getElementById('deletarCat');
 const novoTopModal = document.getElementById('novoTop');
 const editarTopModal = document.getElementById('editarTop');
+const deletarTopModal = document.getElementById('deletarCat');
 
 
 async function exibirTopicosPorCategoria() {
-    var cats = `<div class="accordion" id="catAccordion">
+    let cats = `<div class="accordion" id="catAccordion">
 `;
-    var catData = await axios.get('/interno/categorias').then((catResponse) => catResponse.data).catch(function (catError) {
+    const catData = await axios.get('/interno/categorias').then((catResponse) => catResponse.data).catch(function (catError) {
         console.log(catError);
     });
 
-    for (var i = 0; i < catData.length; i++) {
-        cats += `  <div class="accordion-item">
+    for (let i = 0; i < catData.length; i++) {
+        cats += `  <div class="accordion-item" id="cat${catData[i].id}">
     <h2 class="accordion-header" id="catHeader${catData[i].id}">
       <button type="button" class="accordion-button collapsed col" data-bs-toggle="collapse" data-bs-target="#catBody${catData[i].id}" id="catButton${catData[i].id}" aria-expanded="false" aria-controls="catBody${catData[i].id}">
         ${catData[i].nome}
       </button>
-      <button type="button" class="btn btn-primary col" data-bs-toggle="modal" data-bs-target="#criarTop">
+      <button type="button" class="btn btn-primary col" data-bs-toggle="modal" data-bs-target="#criarTop" data-bs-cat="${catData[i].id}">
         Criar Tópico
       </button>
     </h2>
     <div id="catBody${catData[i].id}" class="accordion-collapse collapse" aria-labelledby="catHeader${catData[i].id}">
       <div class="accordion-body">
         <div class="row justify-content-center" id="catList${catData[i].id}">`;
-        var topData = await axios.get(`/interno/topicos/cat/${catData[i].id}/`).then((topResponse) => topResponse.data).catch(function (topError) {
+        const topData = await axios.get(`/interno/topicos/cat/${catData[i].id}/`).then((topResponse) => topResponse.data).catch(function (topError) {
             console.log(topError);
         });
 
-        for (var j = 0; j < topData.length; j++) {
+        for (let j = 0; j < topData.length; j++) {
             cats += `          <div class="col-4" id="top${topData[j].id}">
             <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editarTop">
               <img id="topImagem${topData[j].id}" src="${topData[j].imagem}" class="img-thumbnail">
@@ -60,12 +62,15 @@ async function exibirTopicosPorCategoria() {
 }
 
 
-function imprimeNovaCategoria(catData) {
-    var id = catData.id;
+function exibirNovaCategoria(catData) {
+    const id = catData.id;
 
-    var catStr = `<div class="accordion-item">
+    const catStr = `<div class="accordion-item" id="cat${id}">
   <h2 class="accordion-header" id="catHeader${id}">
     <button type="button" class"accordion-button collapsed col" data-bs-toggle="collapse" data-bs-target="#catBody${id}" id="catButton${id}" aria-expanded="false" aria-controls="catBody=${id}">
+      ${catData.nome}
+    </button>
+    <button type="button" class="btn btn-primary col" data-bs-toggle="modal" data-bs-target="#criarTop" data-bs-cat="${id}">
       Criar Tópico
     </button>
   </h2>
@@ -78,7 +83,7 @@ function imprimeNovaCategoria(catData) {
   </div>
 </div>`
 
-    let list = document.getElementById('listaCat');
+    const list = document.getElementById('listaCat');
 
     if (list.innerHTML === `<p class='text-start'>Ainda não há categorias presentes</p>`)
         list.innerHTML = catStr;
@@ -86,15 +91,15 @@ function imprimeNovaCategoria(catData) {
         list.innerHTML += catStr;
 }
 
-function imprimeNovoTopicoEmCategoria(topData) {
-    var topStr = `<div class="col-4" id="top${topData.id}>
+function exibirNovoTopicoEmCategoria(topData) {
+    const topStr = `<div class="col-4" id="top${topData.id}>
   <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editarTop">
     <img src="${topData.imagem}" class="img-thumbnail">
     <p>${topData.nome}</p>
   </button>
 </div>`
 
-    let list = document.getElementById(`catList${topData.categoria}`);
+    const list = document.getElementById(`catList${topData.categoria}`);
 
     if (list.innerHTML === `<p class='text-start'>Ainda não há tópicos presentes</p>`)
         list.innerHTML = topStr;
@@ -104,22 +109,23 @@ function imprimeNovoTopicoEmCategoria(topData) {
 
 
 export function criarCategoria() {
-    let nome = novaCatModal.querySelector('#novaCatNome').value;
+    const nome = novaCatModal.querySelector('#novaCatNome').value;
 
     axios.post('/interno/categorias', {
         "nome": nome
     }).then(function (response) {
-        imprimeNovaCategoria(response.data);
+        bootstrap.Modal.getInstance(novaCatModal).hide();
+        exibirNovaCategoria(response.data);
     }).catch(function (error) {
         console.log(error);
     });
 }
 
 export function editarCategoria() {
-    let id = editarCatModal.querySelector('#editarCatId').value;
-    let nome = editarCatModal.querySelector('#editarCatNome').value;
+    const id = editarCatModal.querySelector('#editarCatId').value;
+    const nome = editarCatModal.querySelector('#editarCatNome').value;
 
-    axios.put(`/interno/categorias/${nome}`, {
+    axios.put(`/interno/categorias/${id}`, {
         "nome": nome
     }).then(function (response) {
         document.getElementById(`catButton${id}`).innerHTML = response.data.nome;
@@ -128,62 +134,88 @@ export function editarCategoria() {
     });
 }
 
+export function deletarCategoria() {
+    const id = deletarCatModal.querySelector('#deletarCatId').value;
+
+    axios.delete(`/interno/categorias/${id}`).then(function () {
+        document.getElementById(`cat${id}`).remove();
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
 export function criarTopico() {
-    let nome = novoTopModal.querySelector('#novoTopNome').value;
-    let imagem = novoTopModal.querySelector('#novoTopImagem').value;
-    let prioridade = novoTopModal.querySelector('#novoTopPrioridade').value;
-    let categoria = novoTopModal.querySelector('#novoTopCat').value;
+    const nome = novoTopModal.querySelector('#novoTopNome').value;
+    const imagem = novoTopModal.querySelector('#novoTopImagem').value;
+    const prioridade = novoTopModal.querySelector('#novoTopPrioridade');
+    const categoria = novoTopModal.querySelector('#novoTopCat').value;
 
     const payload = {
         nome: nome,
-        prioridade: prioridade
+        prioridade: prioridade.options[prioridade.selectedIndex].text,
     };
     if (imagem !== "")
         payload[imagem] = imagem;
-    if (categoria !== "")
+    if (categoria !== "0")
         payload[categoria] = categoria;
 
     axios.post('/interno/topicos', payload).then(function (response) {
-       if (response.data.categoria !== 'null')
-           imprimeNovoTopicoEmCategoria(response.data);
+        bootstrap.Modal.getInstance(novoTopModal).hide();
+
+        if (response.data.categoria !== 'null')
+            exibirNovoTopicoEmCategoria(response.data);
     }).catch(function (error) {
-       console.log(error);
+        console.log(error);
     });
 }
 
 export function editarTopico() {
-    let id = editarTopModal.querySelector('#editarTopId').value;
-    let nome = editarTopModal.querySelector('#editarTopNome').value;
-    let imagem = editarCatModal.querySelector('#editarTopImagem').value;
-    let prioridade = editarTopModal.querySelector('#editarTopPrioridade').value;
-    let categoria = editarTopModal.querySelector('#editarTopCategoria').value;
-    let ativo = editarTopModal.querySelector('#editarTopAtivo').value;
+    const id = editarTopModal.querySelector('#editarTopId').value;
+    const nome = editarTopModal.querySelector('#editarTopNome').value;
+    const imagem = editarCatModal.querySelector('#editarTopImagem').value;
+    const prioridade = editarTopModal.querySelector('#editarTopPrioridade');
+    const categoria = editarTopModal.querySelector('#editarTopCategoria').value;
+    const ativo = editarTopModal.querySelector('#editarTopAtivo').value;
 
     const payload = {
         id: id,
         nome: nome,
-        prioridade: prioridade,
+        prioridade: prioridade.options[prioridade.selectedIndex].text,
         ativo: ativo
     };
     if (imagem !== "")
         payload[imagem] = imagem;
-    if (categoria !== "")
+    if (categoria !== "0")
         payload[categoria] = categoria;
 
     axios.put(`/interno/topicos/${id}`, payload).then(function (response) {
-        var topData = response.data;
+        const topData = response.data;
         let div = document.getElementById(`top${id}`);
 
-        if ((div != null) && (topData.categoria !== 'null')) {
-            if (topData.categoria === (div.querySelector(`#topCat${id}`).value === topData.categoria)) {
+        if (topData.categoria !== 'null') {
+            if ((div != null) &&
+                (topData.categoria === div.querySelector(`#topCat${id}`).value)) {
                 div.querySelector(`#topImagem${id}`).value = topData.imagem;
                 div.querySelector(`#topNome${id}`).value = topData.nome;
             }
             else
-                imprimeNovoTopicoEmCategoria(topData);
+                exibirNovoTopicoEmCategoria(topData);
         }
 
         if ((div != null) && (div.querySelector(`#topCat${id}`).value !== topData.categoria))
+            div.remove();
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+export function deletarTopico() {
+    const id = deletarTopModal.querySelector('#deletarTopId').value;
+
+    axios.delete(`interno/topicos/${id}`).then(function () {
+        let div = document.getElementById(`top${id}`);
+
+        if (div != null)
             div.remove();
     }).catch(function (error) {
         console.log(error);
